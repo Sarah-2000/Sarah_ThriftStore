5717ead1804987ba49958ca298bd9ca4b23d6666
Admin <svlunawat11@gmail.com>
2020-12-12 11:17:14 +0530

    donate and place order buttons added, activities created and linked.

--- app/src/main/java/com/example/thriftstore/Home.java
+++ app/src/main/java/com/example/thriftstore/Home.java
@@ -2,13 +2,33 @@ package com.example.thriftstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
-
+    Button btnorder,btndonate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnorder=findViewById(R.id.btnorder);
+        btnorder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Home.this,order.class);
+                startActivity(intent);
+            }
+        });
+        btndonate=findViewById(R.id.btndonate);
+        btndonate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Home.this,Donate.class);
+                startActivity(intent);
+            }
+        });
     }
 }
